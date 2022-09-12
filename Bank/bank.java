c4d3fb8b0529d97a0b704f9448bf689d6ca37a27
Prajwal <prajwalhuggi02@gmail.com>
2022-09-12 07:09:27 +0530

    packages and oops

---
+++ Bank/bank.java
@@ -0,0 +1,13 @@
+package Bank;
+java .util.Scanner;
+public class Account{
+    public String name;
+    
+}
+
+public class bank {
+    public static void main(String args[]){
+        public void hello(){
+        System.out.println("Hello jp");
+    }
+}
--- hello.java
+++ hello.java
@@ -1,6 +1,9 @@
+import Bank.bank;
 public class hello {
     public static void main(String args[])
     {
         System.out.println("Hello World");
+        bank b1= new bank();
+        System.out.println(b1);
     }
 }
